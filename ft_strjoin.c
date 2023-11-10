c2f6cd9eb7c1e27a251dba7b90e5e3ea0c71d829
El Mahjoub Bouboul <ebouboul@e1r5p6.1337.ma>
2023-11-10 03:51:59 +0100

    still not finish

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -11,3 +11,65 @@
 /* ************************************************************************** */
 
 
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	size;
+	char	*res;
+
+	if (!*s1 && !*s2)
+		return (NULL);
+
+	size = 0;
+
+
+	if (s1)
+		size += ft_strlen(s1);
+	if (s2)
+		size += ft_strlen(s2);
+
+	res = (char *)malloc(sizeof(char) * (size + 1));
+	if (!res)
+		return (NULL);
+
+	size_t i = 0;
+	while (s1 && s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+
+	size_t j = 0;
+	while (s2 && s2[j])
+	{
+		res[i + j] = s2[j];
+		j++;
+	}
+
+	res[i + j] = '\0';
+
+	return (res);
+}
+
+/*
+#include <stdio.h>
+
+int main() {
+    const char *s1 = "hruy    ";
+    const char *s2 = "gjhgjf";
+    char *result = ft_strjoin(s1, s2);
+
+    if (result != NULL) {
+        printf("String 1: %s\n", s1);
+        printf("String 2: %s\n", s2);
+        printf("Concatenated String: %s\n", result);
+        
+        free(result);
+    } else {
+        printf("Error: Unable to concatenate strings.\n");
+    }
+
+    return 0;
+}
+*/
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,3 +11,42 @@
 /* ************************************************************************** */
 
 
+#include "libft.h"
+
+char    *ft_strtrim(char const *s1, char const *set)
+{
+    size_t    start;
+    size_t    end;
+    char    *trimmed;
+
+    // Check if either input string is NULL
+    if (!*s1 || !*set)
+        return (NULL);
+
+    // Find the starting index by skipping characters in 'set'
+    start = 0;
+    while (s1[start]  &&  *set != s1[start])  
+        start++;
+
+    // If the entire string is trimmed, return an empty string
+    if (s1[start] == '\0')
+        return (strdup(""));
+
+    // Find the ending index by skipping characters in 'set' from the end
+    end = strlen(s1) - 1;
+    while (end > start &&  *set != s1[end] )
+        end--;
+
+    // Calculate the length of the trimmed string
+    size_t len = end - start + 1;
+
+    // Allocate memory for the trimmed string
+    trimmed = (char *)malloc(sizeof(char) * (len + 1)); // +1 for the null terminator
+    if (!trimmed)
+        return (NULL);
+
+    // Copy the trimmed portion to the new string
+   strncpy(trimmed, s1 + start, len + 1);
+
+    return (trimmed);
+}
