0406d7b5e106c2eac86588df53c4d773de87add2
El Mahjoub Bouboul <ebouboul@e2r5p7.1337.ma>
2023-11-11 02:15:02 +0100

    done

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -13,40 +13,49 @@
 
 #include "libft.h"
 
-char    *ft_strtrim(char const *s1, char const *set)
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    size_t    start;
-    size_t    end;
-    char    *trimmed;
+	char		*str;
+	size_t		start;
+	size_t		end;
 
-    // Check if either input string is NULL
-    if (!*s1 || !*set)
-        return (NULL);
+	if (!s1 || !set)
+		return (NULL);
 
-    // Find the starting index by skipping characters in 'set'
-    start = 0;
-    while (s1[start]  &&  *set != s1[start])  
-        start++;
-
-    // If the entire string is trimmed, return an empty string
-    if (s1[start] == '\0')
-        return (strdup(""));
-
-    // Find the ending index by skipping characters in 'set' from the end
-    end = strlen(s1) - 1;
-    while (end > start &&  *set != s1[end] )
-        end--;
-
-    // Calculate the length of the trimmed string
-    size_t len = end - start + 1;
-
-    // Allocate memory for the trimmed string
-    trimmed = (char *)malloc(sizeof(char) * (len + 1)); // +1 for the null terminator
-    if (!trimmed)
-        return (NULL);
-
-    // Copy the trimmed portion to the new string
-   strncpy(trimmed, s1 + start, len + 1);
-
-    return (trimmed);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (end && s1[end] && ft_strchr(set, s1[end]))
+		end--;
+	str = ft_substr(s1, start, end - start + 1);
+	return (str);
 }
+/*
+int	main(void)
+{
+	const char	*input_string;
+	char		*trimmed_string;
+	const char	*trim_set;
+
+	input_string = "hraaahelloekjaaaaff";
+	trim_set = "a";
+	trimmed_string = ft_strtrim(input_string, trim_set);
+	if (trimmed_string)
+	{
+		printf("Original String: \"%s\"\n", input_string);
+		printf("Trimmed String: \"%s\"\n", trimmed_string);
+		free(trimmed_string);
+	}
+	else
+		printf("Memory allocation failed or input was NULL.\n");
+	return (0);
+}
+*/
